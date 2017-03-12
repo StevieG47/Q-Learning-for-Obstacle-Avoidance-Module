0af3a1c2e65adb21892a422103451017abea6c97
StevieG47 <47stevo@gmail.com>
2017-03-12 05:39:31 -0400

    Added test.cpp with unit tests for the class. Tests included for the functions in the class. This is for task 10. Organize code was task 9.

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,31 @@
 #include <gtest/gtest.h>
+#include "Demo.h"
+#include "Demo.cpp"
 
-TEST(dummy, should_pass)
-{
-    EXPECT_EQ(1, 1);
+ TEST(dummy, should_pass)
+ {
+ EXPECT_EQ(1, 1);
+}
+
+
+TEST(QTest,findState) {
+  robot Test;
+  EXPECT_EQ(Test.findState(0, 0), 0);  //check if finding state correctly
+}
+
+TEST(QTest,decideAction) {
+  robot Test;
+  EXPECT_LT(Test.decideAction(1), 3);
+  EXPECT_GT(Test.decideAction(1), -1);  //check if deciding relevant action
+}
+
+TEST(QTest,assignReward) {
+  robot Test;
+  EXPECT_EQ(Test.assignReward(0, true), 10);  //check if assigning reward correctly
+}
+
+TEST(QTest,maxFuture) {
+  robot Test;
+  Test.Q[1][1] = 55;
+  EXPECT_EQ(Test.maxFuture(1), 55);  //check if computing max Future value correctly
 }
