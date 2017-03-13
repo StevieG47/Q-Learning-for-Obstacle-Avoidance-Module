f5873d571359191a6874e47758681254293f7587
StevieG47 <47stevo@gmail.com>
2017-03-13 00:24:32 -0400

    Edited QLearn_Demp.cpp and created Qclass.cpp and Qclass.h to separate demo files from deliverable files, removing errors. Task 12, get rid of errors/warnings.

--- QLearning/Qclass.cpp
+++ QLearning/Qclass.cpp
@@ -1,17 +1,17 @@
-#include "QLearn_Demo.h"
-
-#include "HallwayExample.h"
+#include "Qclass.h"
+//***********************************************************************
+//INCLUDE FILES NEEDED FOR READING DATA FROM SENSORS
+//INCLUDE FILES NEEDED FOR PERFORMING ACTIONS/ACCESSING ACTUATORS
+//***********************************************************************
 #include <iostream>
 #include <stdlib.h>
-
-
 using std::cout;
 using std::endl;
 
 
 
 //DETERMINE STATE
-int robot::findState(int left, int right) {
+int Q::findState(int left, int right) {  //based on sensor value combination find state
   int state = 0;
   if (left == 0 && right == 0) {
     state = 0;
@@ -29,22 +29,22 @@ int robot::findState(int left, int right) {
 }
 
 //DECIDE ACTION
-int robot::decideAction(int state) {
+int Q::decideAction(int state) {
   int action;
   int Rand = rand() % 100;
 
-  //15 percent chance of choosing a random action. Want random action sometimes to explore new possibilities/possible better action
+  //35 percent chance of choosing a random action. Want random action sometimes to explore new possibilities/possible better action
   if (Rand > 65) {
     int randAction = rand() % 3;  //random number between 0 and 2
 
     action = randAction;
-    //cout << "Random action " << action << " was chosen" << endl;  //print we did a random action
+
   }
   //Use Q table to choose highest rated action for current state, do this most of the time
   else {
     int bestAction;  //initiate best action var
     int bestQ = -100000;  //initiate best Q value, this corresponds to a state action pair value
-    //cout << "Best Q: " << bestQ << endl;
+
     for (int i = 0; i < 3; i++) {  //cycle through every action for current state
       if (Q[state][i] > bestQ) {  //if state action pair Q value is greater than current best value
         bestQ = Q[state][i];  //set new highest Q value
@@ -52,17 +52,14 @@ int robot::decideAction(int state) {
 
       }
     }
-    //print we used Q table, what we did
-    //cout << "Q table used to decide action:" << endl;
-    // cout << "For state " << state << ", action " << bestAction << " was chosen."
-    //   << endl << endl;
+
     action = bestAction;
   }
   return action;  //return chosen action
 }
 
 //ASSIGN REWARD
-int robot::assignReward(int prevAction, bool crash) {
+int Q::assignReward(int prevAction, bool crash) {
   int reward;
   if (prevAction == 0) {  //if moved forward reward since we want to explore
     reward = 10;
@@ -72,13 +69,13 @@ int robot::assignReward(int prevAction, bool crash) {
   }
   if (crash)  //if crashed give big negative reward
     reward = -100;
-  //cout << "Reward " << reward << endl;
+
   return reward;
 }
 
 //FIND MAX FUTURE VALUE
 //Need the max future value to update the Q table
-int robot::maxFuture(int state) {
+int Q::maxFuture(int state) {
   int currentMax = -100000;  //initiate var to keep track of max
 
   for (int i = 0; i != 3; i++) {  //cycle through every action
@@ -90,9 +87,9 @@ int robot::maxFuture(int state) {
 }
 
 //UPDATE Q TABLE
-void robot::updateTable(int prevAction, int prevState, bool crash, int state) {
+void Q::updateTable(int prevAction, int prevState, bool crash, int state) {
   double reward = assignReward(prevAction, crash);  //get the reward based on previous action and if crash
-//  cout << "For Q value " << Q[prevState][prevAction] << endl;
+
 
   //Update Q value using equation. Based on old Q value, reward, max Q value of new state, and constants alpha, gamma
   if (crash == true) {  //if crash, the new state doesn't count, it crashed
@@ -106,44 +103,43 @@ void robot::updateTable(int prevAction, int prevState, bool crash, int state) {
           + alpha
           * (reward + (gamma * maxFuture(state))
                   - Q[prevState][prevAction]);
-    // cout << "Reward  " << reward << endl;
+
 
   }
 }
 
 //FUNCTION TO TRAIN ROBOT
-void robot::Train() {
-  // robot Test;      //initiate test var
+void Q::Train() {
+  // Q Test;      //initiate test var
   int firstRun = 0;  //Can't update Q table on first run, no previous data to go off of
   int prevState;
   int prevAction;
 
   //initiate sensors
+  //**********************************
+  //SENSORS INITIALIZED ASSUMING NOTHING IN THE WAY
   int left = 0;
   int right = 0;
+  //**********************************
   bool crash = false;  //initiate not crashing
 
   for (int a = 0; a < 1000; a++) {  //number of iterations to train for
 
-    //If the first run has finished use getLeft and getRight to get sensor values
-    //Can't use these on first run since they require a previous state and previous action
+    
+//************************************************************************************
+//HERE IS WHERE YOUR SENSOR FUNCTIONS WILL GO TO READ VALUE FROM SENSORS EITHER 1 OR 0
     if (firstRun > 0) {
       //Get new sensor values based on what previous state and previous action was
-      //This is for hallway example, see getSensorValues for details
-      left = getLeft(prevState, prevAction);
-      right = getRight(prevState, prevAction);
+      //left = getLeftSensorValue();//representative function
+      //right = getRightSensorValue();//representative function
     }
-
-    //print the sensor values
-    //cout << "sensorLeft: " << left << endl;
-    //cout << "sensorRight: " << right << endl;
+//************************************************************************************
 
     //Determine state based on left and right sensors
-    // int state = Test.findState(left, right);  //set state with sensor vals
-    int state = findState(left, right);
-    //cout << "state: " << state << endl;
+    int state = findState(left, right);//set state with sensor vals
 
-    //CANT UPDATE ON FIRST RUN. IF AFTER THAT UPDATE
+
+    //Will update after first run is complete
     if (firstRun > 0) {
       if ((prevState == 3 && prevAction == 0)
           || (prevState == 1 && prevAction == 0)
@@ -152,17 +148,15 @@ void robot::Train() {
       }
 
       //Update the Q Table based on prevstate, prevaction , and current state
-      // cout << endl;
-      // cout << "Previous Action: " << prevAction << endl;
-      //cout << "Previous state: " << prevState << endl;
-      //cout << "Current state: " << state << endl;
-      //  Test.updateTable(prevAction, prevState, crash, state);  //update the Q table
       updateTable(prevAction, prevState, crash, state);
     }
 
     //Decide action based on current state. Picks either random action or uses table to decide
-    //  int action = Test.decideAction(state);    //decide action based on state
-    int action = decideAction(state);
+     int action = decideAction(state);//decide action based on state
+//*************************************************************************
+//HERE IS WHERE ACTION IS SENT TO ACTUATORS SO APPROPRIATE ACTION IS TAKEN
+//performAction(action);//representative function
+//*************************************************************************
 
     prevState = state;    //set prevstate = state before next iteration
     prevAction = action;    //same
@@ -171,80 +165,60 @@ void robot::Train() {
 
     //if crash reset  robot to first position, keep Q table though
     if (crash == true) {
-      //cout << "CRASH took place previous stage" << endl;
-      crash = false;
+      crash = false;   //resetting so back to false
       firstRun = 0;    //basically start over if there is a crash
+//**************************************************************
+//ROBOT RESETS TO DETECTING NOTHING AFTER A CRASH
+//performAction(reset)//representative function
+//**************************************************************
       left = 0;    //give initial sensor values again
       right = 0;
     }
 
   }
   //PRINT Q TABLE
-  /*
   cout << "Final Q Table: " << endl;
-  cout << Test.Q[0][0] << "  " << Test.Q[0][1] << " " << Test.Q[0][2] << endl;
-  cout << Test.Q[1][0] << " " << Test.Q[1][1] << " " << Test.Q[1][2] << endl;
-  cout << Test.Q[2][0] << " " << Test.Q[2][1] << " " << Test.Q[2][2] << endl;
-   cout << Test.Q[3][0] << " " << Test.Q[3][1] << " " << Test.Q[3][2] << endl;*/
-  cout
-      << "Demo for a robot with left/right sensors in a narrow hallway. The available actions of the"
-
-      " robot are move forward, turn 45 deg left in place, and turn 45 deg right in place. Turning"
-
-      " once to the right  will lead to the robot sensing something on the right and nothing on the left."
-
-      " Turning to the right again will lead to both sensors detecting, since there is a wall in front."
-      " The robot will crash if it moves forward while detecting anything, meaning it will only be able to move forward"
-
-      " without crashing by turning until it is parallel with the walls (detecting nothing)."
-      << endl;
-
-      
-  cout << endl << "Final Q Table: " << endl;
-  cout << " " << Q[0][0] << " " << Q[0][1] << " " << Q[0][2] << endl;
-  cout << Q[1][0] << " " << Q[1][1] << " " << Q[1][2] << endl;
-  cout << Q[2][0] << " " << Q[2][1] << " " << Q[2][2] << endl;
-  cout << Q[3][0] << " " << Q[3][1] << " " << Q[3][2] << endl;
+  cout << " " << Q[0][0] << " " << Q[0][1] << " " << Q[0][2];
+  cout << endl;
+  cout << Q[1][0] << " " << Q[1][1] << " " << Q[1][2];
+  cout << endl;
+  cout << Q[2][0] << " " << Q[2][1] << " " << Q[2][2];
+  cout << endl;
+  cout << Q[3][0] << " " << Q[3][1] << " " << Q[3][2];
+  cout << endl;
   cout
       << "States: 0. detect nothing, 1, detect right, 2. detect left, 3. detect both."
-      << endl;
-
+       << endl;
   cout
-      << "Actions: 0. Move forward, 1. turn right in place, 2. turn left in place."
-      << endl;
-
-  cout << endl;
-  cout
-      <<
-      "After training, the robot has a best action for each state. For state"
-      " 0, detect nothing, the best action is to move forward. For state 1, detect right,"
-
-      " the best action is to turn left. For state 2, detect left, the best action is"
-      " to turn right. And for state 3, detect both, the best action is to turn right.";
-  cout << endl;
-
+      << "Actions: 0. Move forward, 1. turn right in place, 2. turn left in place.";
 }
 
 //USE Q TABLE TO PERFORM
-void robot::Perform() {
+void Q::Perform() {
   //initiate sensor values
   int left = 1;
   int right = 1;
   //no previous action/previous state on first run
   int firstRun = 0;
-  //intiitae prevState, prevAction
+  //initiate prevState, prevAction
   int prevState = 0;
   int prevAction = 0;
   cout << endl << endl << "Performance using Q Table:" << endl;
 
-  for (int a = 0; a < 5; a++) {    //iterate 5 times
+//*****************************************************************************
+//DEPENDING ON WHAT PERFORMANCE IS WANTED, ITERATION CAN BE ADJUSTED
+  for (int a = 0; a < 5; a++) {    //iterate 5 times, do 5 actions
+//*****************************************************************************
 
-  //Get new sensor values based on what previous state and previous action was
-  //This is for hallway example, see HallwayExample.h for details
+
+//************************************************************************************
+//HERE IS WHERE YOUR SENSOR FUNCTIONS WILL GO TO READ VALUE FROM SENSORS EITHER 1 OR 0
+//Get new sensor values  
   if (firstRun > 0) {
-    left = getLeft(prevState, prevAction);
-    right = getRight(prevState, prevAction);
+      // left = getSensorLeft;//representative function
+      //right = getSensorRight;//representative function
   }
+//***********************************************************************************
 
   //Determine state based on left and right sensors
     int state2 = findState(left, right);  //set state with sensor vals
@@ -259,6 +233,12 @@ void robot::Perform() {
 
     }
   }
+  
+//*************************************************************************
+//HERE IS WHERE ACTION IS SENT TO ACTUATORS SO APPROPRIATE ACTION IS TAKEN
+//performAction(bestAction);//representative function
+//*************************************************************************
+  
     cout << "For state " << state2 << ", action " << bestAction
          << " was taken"
          << endl;
@@ -266,13 +246,6 @@ void robot::Perform() {
     prevAction = bestAction;
     firstRun = 1;  //done with first run
   }
-  cout
-      << endl;
-  cout
-      << "Using the Q table, the robot turns away until it is parallel with the walls"
-
-      " (or detects nothing). It then goes straight."
-      << endl;
   
 }
 
